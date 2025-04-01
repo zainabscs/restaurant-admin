d0c215a8c6f971e6659c7c848bfadd50338186ca
Zaina Azam <zainaazam23@gmail.com>
2025-04-01 21:46:40 +0500

    Added admin on git

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
+import { Route, Routes, Navigate } from 'react-router-dom'
+import Add from './pages/Add/Add'
+import List from './pages/List/List'
+import Orders from './pages/Orders/Orders'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+
+  const url = 'http://localhost:4000'
+  return (
+    <div>
+      <ToastContainer/>
+      <Navbar/>
+      <hr />
+      <div className="app-content">
+        <Sidebar/>
+        <Routes>
+          <Route path="/" element={<Navigate to="/orders" />} /> {/* Redirects to /orders */}
+          <Route path='/add' element={<Add url={url}/>}></Route>
+          <Route path='/list' element={<List url={url}/>}></Route>
+          <Route path='/orders' element={<Orders url={url}/>}></Route>
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/List/List.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react'
+import './List.css'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
+const List = ({url}) => {
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/food/list`)
+
+    if (response.data.success) {
+      setList(response.data.data);
+    }
+    else {
+      toast.error("Error");
+    }
+  }
+
+  const removeFood=async(foodId)=>{
+    const response=await axios.post(`${url}/api/food/remove`,{id:foodId});
+    await fetchList();
+    if(response.data.success){
+      console.log("entered");
+      toast.success("Food item removed");
+    }
+    else{
+      toast.error(response.data.message);
+    }
+    }
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+
+  return (
+    <div className='list add flex-col'>
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {
+          list.map((item,index)=>{
+            return(
+              <div className="list-table-format" key={index}>
+                <img src={`${url}/images/`+item.image} alt="" />
+                <p>{item.name}</p>
+                <p>{item.category}</p>
+                <p>${item.price}</p>
+                <p onClick={()=>removeFood(item._id)} className='cursor'>x</p>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  )
+}
+
+export default List
