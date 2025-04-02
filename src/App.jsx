b4c3eaa851b23f9b4ba7d0bd7cf6a9ec2ac9b050
zainabscs <zaina.bscs4707@iiu.edu.pk>
2025-04-02 14:52:12 +0500

    shbs

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
 
   // const url = 'https://restaurant-b-one.vercel.app'
-  const url = 'htps://restaurant-b-one.vercel.app';
+  const url = 'https://restaurant-b-one.vercel.app';
   return (
     <div>
       <ToastContainer/>
